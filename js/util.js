b4b40e5e36966c79c59ea6a965fab251b6e20ba5
Jason Carrete <jasoncarrete5@gmail.com>
2017-11-28 17:24:14 -0500

    Moved helper functions to util.js

---
+++ js/util.js
@@ -0,0 +1,30 @@
+// query format: http://api.wunderground.com/api/[api-key]/[data-type]/q/[state-abbr]/[city].json
+// autocomplete api: http://autocomplete.wunderground.com/aq?query=[query]
+var apiUrl = "http://api.wunderground.com/api/"+apikey;
+
+function parseAutocompleteResults(data) {
+	console.log("Parsing autocomplete results");
+	console.log(data);
+}
+
+function autocomplete(searchText) {
+	var search = "http://autocomplete.wunderground.com/aq?cb=parseAutocompleteResults&query="+searchText;
+
+	// Using jsonp to retrieve and parse autocomplete results
+	var script = document.createElement("script");
+    script.src = search;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+// Gets the location and passes location information to callback once received
+function getLocation(callback) {
+	navigator.geolocation.getCurrentPosition(function(pos) {
+    	var lat = pos.coords.latitude;
+		var long = pos.coords.longitude;
+		$.get(apiUrl+"/geolookup/q/"+lat+","+long+".json", null, function(data, textStatus, jqXHR) {
+			console.log("geolookup status: "+textStatus);
+			console.log(data);
+			callback(data);
+		});
+	});
+}
