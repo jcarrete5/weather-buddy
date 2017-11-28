83f2fb3f671e398d0ca688421c6b2f91e5667863
Jason Carrete <jasoncarrete5@gmail.com>
2017-11-28 17:02:32 -0500

    check if loc-query is also empty string before trying to use geolocation

--- js/current-forecast.js
+++ js/current-forecast.js
@@ -10,16 +10,16 @@ function autocomplete(searchText) {
 	var search = "http://autocomplete.wunderground.com/aq?cb=parseAutocompleteResults&query="+searchText;
 
 	// Using jsonp to retrieve and parse autocomplete results
-	var script = document.createElement('script');
+	var script = document.createElement("script");
     script.src = search;
-    document.getElementsByTagName('head')[0].appendChild(script);
+    document.getElementsByTagName("head")[0].appendChild(script);
 }
 
 $(document).ready(function() {
 	var apiUrl = "http://api.wunderground.com/api/"+apikey;
 
 	var queryStr = new URLSearchParams(window.location.search);
-	if (!queryStr.has("loc-query")) {
+	if (!queryStr.has("loc-query") || queryStr.has("loc-query") && queryStr.get("loc-query") === "") {
 		console.log("no loc-query string; using geolocation");
 
 		// If there is no location query, use client location
@@ -29,7 +29,7 @@ $(document).ready(function() {
 			$.get(apiUrl+"/geolookup/q/"+lat+","+long+".json", null, function(data, textStatus, jqXHR) {
 				console.log("geolookup status: "+textStatus);
 				console.log(data);
-				$.get(apiUrl+"/forecast"+data.location.l+".json", null, function(data, textStatus, jqXHR) {
+				$.get(apiUrl+"/conditions"+data.location.l+".json", null, function(data, textStatus, jqXHR) {
 					console.log("weather information status: "+textStatus);
 					console.log(data);
 				});
@@ -37,5 +37,7 @@ $(document).ready(function() {
 		});
 	} else {
 		console.log("loc-query present; using it");
+
+		autocomplete(queryStr.get("loc-query"));
 	}
 });
