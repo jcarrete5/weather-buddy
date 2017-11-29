690eaee82b4f60ffca2967c08555d1969b2ec37a
Jason Carrete <jasoncarrete5@gmail.com>
2017-11-29 17:32:56 -0500

    Weather icon displays

--- js/current-forecast.js
+++ js/current-forecast.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
-	var weatherData;
+	function updatePage(weatherData) {
+		// Update webpage to show relevant weather information
+		$("#weatherIcon")
+			.attr("src", weatherData.icon_url)
+			.attr("alt", "weather: "+weatherData.icon);
+	}
+
 	var urlParams = new URLSearchParams(window.location.search);
 	if (urlParams.has("wuQuery") && urlParams.get("wuQuery") !== "") {
 		$.get(apiUrl+"/conditions"+urlParams.get("wuQuery")+".json", null, function(data, textStatus, jqXHR) {
 			console.log("weather information status: "+textStatus);
 			console.log(data);
-			weatherData = data;
+			updatePage(data.current_observation);
 		});
 	} else {
 		getLocation(function(loc) {
 			$.get(apiUrl+"/conditions"+loc.location.l+".json", null, function(data, textStatus, jqXHR) {
 				console.log("weather information status: "+textStatus);
 				console.log(data);
-				weatherData = data;
+				updatePage(data.current_observation);
 			});
 		});
 	}
