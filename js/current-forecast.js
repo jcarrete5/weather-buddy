4aace11eec491d9a37a023bcb53051a5254e2e1f
Jason Carrete <jasoncarrete5@gmail.com>
2017-11-29 18:07:33 -0500

    Added weather data to display

--- js/current-forecast.js
+++ js/current-forecast.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
 	function updatePage(weatherData) {
 		// Update webpage to show relevant weather information
+		$("#weatherLocation")
+			.html(weatherData.display_location.full);
 		$("#weatherIcon")
 			.attr("src", weatherData.icon_url)
-			.attr("alt", "weather: "+weatherData.icon);
+			.attr("alt", "weather: "+weatherData.weather);
+		$("#weatherConditions")
+			.append($("<li></li>")
+				.html("Temperature: "+weatherData.temperature_string))
+			.append($("<li></li>")
+				.html("Feels like "+weatherData.feelslike_string))
+			.append($("<li></li>")
+				.html("Dewpoint: "+weatherData.dewpoint_string))
+			.append($("<li></li>")
+				.html("Humidity: "+weatherData.relative_humidity))
+			.append($("<li></li>")
+				.html("Wind: "+weatherData.wind_string))
+			.append($("<li></li>")
+				.html("Wind Chill: "+weatherData.windchill_string))
+			.append($("<li></li>")
+				.html(weatherData.observation_time));
+
+		// Give inner div appearence of being raised
+		$("#inner").css("box-shadow", "1px 1px 5px #62757f");
 	}
 
 	var urlParams = new URLSearchParams(window.location.search);
