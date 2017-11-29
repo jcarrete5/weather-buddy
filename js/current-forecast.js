4c86bcb36a51499d5d513984bb1f5b26b2a36ccd
Jason Carrete <jasoncarrete5@gmail.com>
2017-11-29 14:33:48 -0500

    Started current condition display setup

--- js/current-forecast.js
+++ js/current-forecast.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
+	var weatherData;
 	var urlParams = new URLSearchParams(window.location.search);
 	if (urlParams.has("wuQuery") && urlParams.get("wuQuery") !== "") {
 		$.get(apiUrl+"/conditions"+urlParams.get("wuQuery")+".json", null, function(data, textStatus, jqXHR) {
 			console.log("weather information status: "+textStatus);
 			console.log(data);
+			weatherData = data;
 		});
 	} else {
 		getLocation(function(loc) {
 			$.get(apiUrl+"/conditions"+loc.location.l+".json", null, function(data, textStatus, jqXHR) {
 				console.log("weather information status: "+textStatus);
 				console.log(data);
+				weatherData = data;
 			});
 		});
 	}
