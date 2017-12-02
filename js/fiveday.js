58e53c524239719b9c08ecad10e20df48b61c1e9
Jason Carrete <jasoncarrete5@gmail.com>
2017-12-02 14:08:42 -0500

    Started populating fiveday.html with weather information

--- js/fiveday.js
+++ js/fiveday.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+	function updatePage(weatherData) {
+		var daily = weatherData.forecast.simpleforecast.forecastday;
+		$("#mainContent>div.inlineContainer>div").each(function(i, elm) {
+			$(elm)
+				.append($("<h3></h3>")
+					.html(daily[i].date.weekday_short));
+		});
+	}
 
+	queryWeatherInformation("forecast10day", updatePage);
 });
