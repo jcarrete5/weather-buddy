9b31992c09c8a60fd9c7f7e9412724ac82dd874b
Jason Carrete <jasoncarrete5@gmail.com>
2017-12-04 22:33:55 -0500

    Displaying severe weather alerts

--- js/alerts.js
+++ js/alerts.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-	
+	function updatePage(weatherData) {
+		$("#loadingGif").attr("hidden", "true");
+
+		var alerts = weatherData.alerts;
+		for (let alert of alerts) {
+			$("#alertList")
+				.css("list-style-type", "none")
+				.append($("<li></li>")
+					.append($("<h3></h3>")
+						.html(alert.description))
+					.append($("<p></p>")
+						.html(alert.message)
+						.css("text-align", "left"))
+					.append($("<p></p>")
+						.html(alert.date)
+						.css("font-style", "italic")
+						.css("font-size", "smaller")));
+		}
+	}
+
+	queryWeatherInformation("alerts", updatePage);
 });
